5e43dab44a0bd136f677a5934d8a5f24f4b1f38e
oponn <oponn@umich.edu>
2021-01-03 17:34:36 -0500

    black react logo

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, { useState, lazy, Suspense } from 'react';
 import { Switch, Route, Link, BrowserRouter, NavLink } from "react-router-dom";
 import logo from './logo.svg';
+// import logo from './Rei_Bike_Shorts.png'
 import './App.css';
 
 import Home from './components/Home';
 import InstagramFeed from './components/InstagramFeed';
 import Curation from './components/Curation';
+import Store from './components/Store';
 import PageNotFound from './components/404'
 
 const CounterComponent = () => {
@@ -13,23 +15,25 @@ const CounterComponent = () => {
 
   return (
     <div>
-      Hello Guv {count}
+      Click this to see the number go up: {count}
       <br />
       <button onClick={() => setCount(count => {
-        console.log("Hullo", count)
+        // console.log("Hullo", count)
         return (count + 1)
-      })}>Click</button>
+      })}>I have too much time on my hands</button>
     </div>
   );
 }
 
 const Header = () => {
   return (
-    <header>
+    <header className='header'>
       <h1>Oponn</h1>
       <NavLink to="/" exact={true} activeClassName='selected-link' className='header-links home'>Home</NavLink>
-      <NavLink to="/instagramfeed" activeClassName='selected-link' className='header-links'>InstagramFeed</NavLink>
+      <NavLink to="/instagramfeed" activeClassName='selected-link' className='header-links'>Feed</NavLink>
       <NavLink to="/curation" activeClassName='selected-link' className='header-links'>Curation</NavLink>
+      <NavLink to="/store" activeClassName='selected-link' className='header-links'>Store</NavLink>
+      <hr className='line'/>
     </header>
   )
 }
@@ -47,26 +51,27 @@ const App = () => {
             <Route path="/" component={Home} exact={true} />
             <Route path="/instagramfeed" component={InstagramFeed} />
             <Route path="/curation" component={Curation} />
+            <Route path="/store" component={Store} />
             <Route component={PageNotFound} />
           </Switch>
         </div>
       </BrowserRouter>
       <div className="App">
         <header className="App-header">
-          <CounterComponent />
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Edit <code>src/App.js</code> and save to reload.
-        </p>
-          <a
+            This website was built in react
+          </p>
+          {/* <a
             className="App-link"
             href="https://reactjs.org"
             target="_blank"
             rel="noopener noreferrer"
-          >
+            >
             Learn React
-        </a>
+          </a> */}
         </header>
+          <CounterComponent />
       </div>
     </div>
   );
---
+++ src/components/Store.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Store() {
+  return (
+    <div className="container-main">
+      This is my web store
+    </div>
+    )
+}
